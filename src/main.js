f5ed03b8cc758f252f6b54901c536730e34f10e2
TatiYas <iasinovska_lawyer@ukr.net>
2024-05-26 22:16:14 +0200

    Update_6

--- src/main.js
+++ src/main.js
@@ -1,45 +1,57 @@
+import { getPhotos } from './js/pixabay-api';
+import { createMarkup } from './js/render-functions';
 // Описаний у документації
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-import { fetchImage } from './js/pixabay-api';
-import { renderImages } from './js/render-functions';
 // Описаний у документації
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// ................................................
-const loader = document.getElementById('loader');
-const gallery = document.querySelector('.gallery');
-const form = document.querySelector('.form');
-const input = document.querySelector('input');
-form.addEventListener('submit', async evt => {
-  evt.preventDefault();
-  const query = input.value.trim();
-
-  if (!query) {
-    iziToast.error({
-      title: 'Error',
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      position: 'topRight',
-    });
-    return;
-  }
-
-  loader.classList.remove('hidden');
-  gallery.innerHTML = '';
-
-  const images = await fetchImage(query);
-  renderImages(images);
-  new SimpleLightbox('.gallery a', {
-    animationSpeed: 200,
-    animationSlide: true,
-    disableScroll: false,
-    history: false,
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
-
-  loader.classList.add('hidden');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
+
+const formElement = document.querySelector('.search-form');
+formElement.addEventListener('submit', onSubmit);
+const listElement = document.querySelector('.gallery');
+const loader = document.querySelector('.wraper-loader');
+
+function onSubmit(event) {
+  event.preventDefault();
+  listElement.innerHTML = '';
+  showLoader();
+  const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+  getPhotos(searchQuery)
+    .then(response => {
+      if (response.hits.length === 0) {
+        return iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: ' #fff',
+          backgroundColor: '#ef4040',
+          position: 'bottomRight',
+          messageSize: '16px',
+          messageLineHeight: '100%',
+          iconColor: 'white',
+        });
+      }
+
+      listElement.insertAdjacentHTML('beforeend', createMarkup(response.hits));
+      lightbox.refresh();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
+
+function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+function hideLoader() {
+  loader.classList.add('is-hidden');
+}
