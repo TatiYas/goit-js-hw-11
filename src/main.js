ec89036ef37b78c504349c1c3b8d01cdc1f439a7
TatiYas <iasinovska_lawyer@ukr.net>
2024-05-26 23:24:05 +0200

    Update_11

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import { createMarkup } from './js/render-functions';
 // Описаний у документації
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import 'izitoast/dist/css/iziToast.min.css';
+import "izitoast/dist/css/iziToast.min.css";
 // Описаний у документації
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
